b8337574b4eb7f7d9fa05a76cedd6e8588a83fb4
nickzaf95 <nicholaszafiriou@gmail.com>
2020-04-22 15:00:30 +0200

    Done.

--- src/components/YouTubeDebugger.js
+++ src/components/YouTubeDebugger.js
@@ -1,1 +1,58 @@
 // Code YouTubeDebugger Component Here
+import React, { Component } from 'react';
+
+export default class DigitalClicker extends Component{
+    constructor(){
+        super()
+
+        this.state = {
+            errors: [],
+            user: null,
+            settings: {
+                bitrate: 8,
+                video: {
+                    resolution: '1080p'
+                }
+            }
+        }
+    }
+
+    handleBitrate = () => {
+        this.setState({
+            ...this.state,
+            settings: {
+                ...this.state.settings,
+                bitrate: 12
+            }
+        })
+    }
+
+    handleRes = () => {
+        this.setState({
+            ...this.state,
+            settings:{
+                ...this.state.settings,
+                video: {
+                    resolution: "720p"
+                }
+            }
+        })
+    }
+
+    render(){
+        return (
+
+            <div>
+                <button
+                className="bitrate"
+                onClick={this.handleBitrate}>
+                </button>
+
+                <button
+                className="resolution"
+                onClick={this.handleRes}>
+                </button>
+            </div>
+        )
+    }
+}
